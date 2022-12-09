234cb964dafd7f7ed470d2bf46fc9ec219bbb936
aleksp7486 <aleksandrpopov7486@gmail.com>
2022-12-09 15:43:03 +0300

    imageGallery spread otherProps

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -2,13 +2,13 @@ import PropTypes, { arrayOf } from 'prop-types';
 import { Gallery } from './ImageGallery.styled';
 import ImageGalleryItem from 'components/ImageGalleryItem';
 
-const ImageGallery = ({ images, onClick }) => {
+const ImageGallery = ({ images, ...otherProps }) => {
   return (
     <Gallery>
       {images.map(el => (
         <ImageGalleryItem
           key={el.id}
-          onClick={onClick}
+          {...otherProps}
           webFormatURL={el.webformatURL}
           largeImageURL={el.largeImageURL}
           tags={el.tags}
